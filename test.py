db555c4fb6ecdc4904a06ca95ee67e4332712bb0
lbartell <lenabartell@gmail.com>
2016-07-06 23:44:31 -0400

    Create script to test an instance of DDEStack
    Create and test an instance of the DDEStack class for debugging,
    profiling, etc.

--- test.py
+++ test.py
@@ -1,14 +1,16 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sat Jun 25 22:36:31 2016
 
-@author: Lena
-"""
+# Imports
+from lena_DDE import DDEStack
 
-from sys import stdout
-from time import sleep
-for i in range(1,20):
-    stdout.write("\r%d" % i)
-    stdout.flush()
-    sleep(1)
-stdout.write("\n")
+# Create DDE stack
+kwargs = {
+    'filename': 'test_short.tif',
+    'regionsize': 51,
+    'regionspacing': 100,
+    'euler': False,
+    }
+stack = DDEStack(**kwargs)
+
+# Displace grid displacements, saving each frame
+basename = '\test_output\frame'
+stack.show_displacements(basename=basename)
