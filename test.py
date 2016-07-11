28c40743a4d2f6494688280fe3504004fa66efaa
lbartell <lenabartell@gmail.com>
2016-07-11 17:29:28 -0400

    Change testing parameters
    Change parameters, just to play around.

--- test.py
+++ test.py
@@ -6,11 +6,11 @@ from lena_DDE import DDEStack
 # Create test DDE stack
 LMkwargs = {'damping': 1.,
             'max_iter': 20,
-            'ptol': 1e-7}
+            'ptol': 1e-6}
 kwargs = {
-    'filename': 'test_short2_crop.tif',
+    'filename': 'test_crop.tif',
     'regionsize': 25,
-    'regionspacing': 300,
+    'regionspacing': 25,
     'euler': False,
     'LMkwargs': LMkwargs
     }
@@ -19,9 +19,7 @@ stack = DDEStack(**kwargs)
 # Displace grid displacements, saving each frame
 basename = 'test_output/frame'
 savekwargs = {'dpi': 150}
-stack.show_deformation(basename=basename, savekwargs=savekwargs, showim=True,
-                       showboxes=True, showstrain=True, straincolormap='bwr',
-                       xlim=(0, 512), ylim=(0, 512))
+stack.show_deformation(basename=basename, savekwargs=savekwargs, 
 
 ## Profile DDEStack creation via cProfile
 #cProfile.run('DDEStack(**DDEparams)', 'runstats', sort='cumulative')
